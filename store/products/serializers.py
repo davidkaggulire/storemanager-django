eff66669289a576b7d570c1da0914eda7a567f1a
davidkaggulire <dkaggulire@gmail.com>
2022-01-04 21:38:16 +0300

    edit serializers

--- store/products/serializers.py
+++ store/products/serializers.py
@@ -5,4 +5,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name', 'category', 'image', 'price', 'quantity']
+        fields = ['id', 'name', 'category', 'image', 'price', 'price_currency', 'quantity']
