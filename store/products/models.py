6f721400b2c6f3fe09069ce83f9d9ae61124fe81
davidkaggulire <dkaggulire@gmail.com>
2022-01-04 21:37:57 +0300

    edit product models

--- store/products/models.py
+++ store/products/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
+from category.models import Category
+
 
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
-    category = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(null=False, blank=False)
+    category = models.ForeignKey(Category, null=False, on_delete=models.CASCADE)
     image = models.CharField(max_length=255, null=True, blank=True)
     price = MoneyField(max_digits=19, decimal_places=2, null=True, default_currency='UGX')
     quantity = models.IntegerField()
