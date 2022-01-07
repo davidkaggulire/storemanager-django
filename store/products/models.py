98000d800c6ac8ebde0e49b9bc589d08f5ade38f
davidkaggulire <dkaggulire@gmail.com>
2022-01-07 13:11:31 +0300

    edit models

--- store/products/models.py
+++ store/products/models.py
@@ -6,7 +6,7 @@ from category.models import Category
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
-    description = models.TextField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
     category = models.ForeignKey(Category, null=False, on_delete=models.CASCADE)
     image = models.CharField(max_length=255, null=True, blank=True)
     price = MoneyField(max_digits=19, decimal_places=2, null=True, default_currency='UGX')
