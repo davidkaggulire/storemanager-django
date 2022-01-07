4e2bc2b4338e368823cea7b92a15a3eab1f56c38
davidkaggulire <dkaggulire@gmail.com>
2022-01-07 13:11:09 +0300

    edit category models

--- store/category/models.py
+++ store/category/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.name
